6fdf155f68c7d29705159b96d5995617c221cc9b
Javier del Valle <jvalle@protonmail.com>
2019-08-31 02:23:56 +0200

    controlling errors and reusing ports

--- ProxyBroker.dart
+++ ProxyBroker.dart
@@ -7,18 +7,18 @@ import 'Protocol.dart';
 
 class ProxyBroker {
 
-  ServerSocket mainSocket = null;
-  Map workers = {};  // port->socket
+  ServerSocket mainSocket;
+  Map workers = {};
   List usedForwardingPorts = [];
   
   bool start(port, {Function onDone, Function onError}) {
 
     if (onDone == null) {
-      onDone = () => {print('server started on port $port')};
+      onDone = () => print('server started on port $port');
     }
 
     if (onError == null) {
-      onError = () => {print('error stopping server')};
+      onError = () => print('error stopping server');
     }
 
     // request the OS to bind a new socket to the specified port
@@ -27,12 +27,18 @@ class ProxyBroker {
       mainSocket = server;
 
       // listen for incoming connections
-      mainSocket.listen((Socket socket) {
+      mainSocket.listen((Socket controlSocket) {
 
-        Socket remoteSocket = null;
+        var workerId;
+
+        controlSocket.done.then((socket) {
+          print('control socket for worker: $workerId fully disconnected');
+        }).catchError((error) {
+          print('error in controlSocket for worker: $workerId');
+        });
 
         // listen for incoming data
-        socket.listen((List<int> data) {
+        controlSocket.listen((List<int> data) {
 
           print('incoming ${data.length} bytes on main socket');
 
@@ -40,125 +46,154 @@ class ProxyBroker {
 
           if (msg == null) {
           	print('invalid msg, closing connection');
-          	socket.close();
+          	controlSocket.close();
           	return;
 
           } else if (msg['version'] == 0xff && msg['cmdCode'] == CMD_REGISTER_WORKER) {
-			// The port received from the worker in the register will be
-			// used as worker id: The broker will open that port, and all
-			// data comming to it will be forwarded to that worker 
+            // The port received from the worker in the register will be
+            // used as worker id: The broker will open that port, and all
+            // data comming to it will be forwarded to that worker 
 
-          	var workerId = msg['port'];
-          	print('new worker requesting to listen at port: ${workerId}');
+          	print('new worker requesting to listen at port: ${msg['port']}');
 
-          	if (workers.containsKey(workerId)) {
-          		var resp = buildRegisterRejectedResponse(workerId);
+          	if (workers.containsKey(msg['port'])) {
+          		var resp = buildRegisterRejectedResponse(msg['port']);
           		print('error, a worker has already been registered for that port');
-          		socket.add(resp);
+          		controlSocket.add(resp);
           		// TODO: close connection here? or wait for another msg?
-          		// socket.close(); 
+          		// controlSocket.close(); 
           		return;
           	}
 
-			workers[workerId] = {'controlSocket': socket};
+            workerId = msg['port'];
 
-			// open the workerId port and listen (connections to that port will be forwarded to the worker)
-			ServerSocket.bind('0.0.0.0', workerId).then((ServerSocket server) {
+			      workers[workerId] = {'controlSocket': controlSocket,
+                                 'forwardingSockets': [],
+                                 'browserSockets': [],
+                                 'proxySocket': null};
 
-				// wait for new client requesting a connection
-				server.listen((Socket browserSocket) {
-					print('new client for proxyPort $workerId');
+            // open the workerId port and listen (connections to that port will be forwarded to the worker)
+            ServerSocket.bind('0.0.0.0', workerId).then((ServerSocket server) {
+              
+              print('worker listening at port: $workerId');
+              
+              workers[workerId]['proxySocket'] = server;
 
-					browserSocket.done.catchError((error) => print('error on browserSocket for workerId: $workerId, : $error'));
+				      // wait for new client requesting a connection
+              server.listen((Socket browserSocket) {
+                print('new client for proxyPort $workerId');
 
-					// request a forwarding connection to the worker
-					requestForwardingConn(workerId, onDone: (workerSocket) {
+                // save socket to close it later
+                workers[workerId]['browserSockets'].add(browserSocket);
 
-						// listen browserSocket for incoming data and forward it to the worker
-						browserSocket.listen((List<int> data) {
-							print('${data.length} bytes of data received from browser in port $workerId, forwarding them to the worker');
-							try {
-								workerSocket.add(data);
-							} on SocketException {
-								print('socket exception while adding data');
-							} catch (e) {
-								print('caught exception while adding data to socket: $e');
-							}
-						}, onError: (e) {
-							print('error listening from browserSocket: $e');
-							if (e.osError.errorCode == 104) {
-								// Connection reset by peer, errno = 104
-								print('connection reset by peer, closing socket');
-								browserSocket.close();
-							}
-						});
+                browserSocket.done.catchError((error) => print('error on browserSocket for workerId: $workerId, : $error'));
 
-						// listen workerSocket for incoming data and forward it to the browser
-						workerSocket.listen((List<int> data) {
-							print('${data.length} bytes of data received from worker, sending it back to the browser');
-							try {
-								browserSocket.add(data);
-							} on SocketException {
-								print('socket exception while adding data');
-							} catch (e) {
-								print('caught exception while adding data to socket: $e');
-							}
-						}, onError: (e) => print('error listening from workerSocket: $e'));
-					});
+                // request a forwarding connection to the worker
+                requestForwardingConn(workerId, onDone: (workerForwSocket) {
 
-				}, onError: (error) => print('error while listening for connections in proxy socket for workerId: $workerId. Error: $error'));
+                  // listen browserSocket for incoming data and forward it to the worker
+                  browserSocket.listen((List<int> data) {
+                    print('${data.length} bytes of data received from browser in port $workerId, forwarding them to the worker');
+                    try {
+                      workerForwSocket.add(data);
+                    } on SocketException {
+                      print('socket exception while adding data');
+                    } catch (e) {
+                      print('caught exception while adding data to socket: $e');
+                    }
+                  }, onError: (e) {
+                    print('error listening from browserSocket: $e');
+                    if (e.osError.errorCode == 104) {
+                      // Connection reset by peer, errno = 104
+                      print('connection reset by peer, closing socket');
+                      browserSocket.close();
+                    }
+                  });
 
-			}, onError: (error) => print('error binding socket for workerId: $workerId. Error: $error'));
+                  // listen workerSocket for incoming data and forward it to the browser
+                  workerForwSocket.listen((List<int> data) {
+                    print('${data.length} bytes of data received from worker, sending it back to the browser');
+                    try {
+                      browserSocket.add(data);
+                    } on SocketException {
+                      print('socket exception while adding data');
+                    } catch (e) {
+                      print('caught exception while adding data to socket: $e');
+                    }
+                  }, onError: (e) => print('error listening from workerForwSocket: $e'));
+                });
 
-			// send register success msg to the worker
-      		var resp = buildRegisterSuccessResponse(workerId);
-      		socket.add(resp);
+              }, onError: (error) => print('error while listening for connections in proxy socket for workerId: $workerId. Error: $error'));
+
+            }, onError: (error) => print('error binding socket for workerId: $workerId. Error: $error'));
+
+            // send register success msg to the worker
+            var resp = buildRegisterSuccessResponse(workerId);
+            controlSocket.add(resp);
           }
 
-        }, onError: (error) => print('error on listen() for incoming data on dedicated socket'));
+        }, onDone: () {
+          // worker controlSocket closed
+          print('closed controlSocket for worker $workerId');
+          removeWorker(workerId);
+        }, onError: (error) {
+          print('error on listen() for incoming data on controlSocket, workerId: $workerId');
+          removeWorker(workerId);
+        });
 
       }, onError: (error) => print('error while listening for connections in main socket'));
 
       onDone();
 
-    }).catchError((error) => {onError()});
+    }).catchError((error) => onError());
 
     return true;
   }
 
   int chooseNewForwardingPort() {
-  	var BASE_PORT = 7000;
-  	var MAX_TRIES = 100;
+
+  	const BASE_PORT = 7000;
 
   	for (var i=0 ; i<=1000; i++) {
   		var chosenPort = BASE_PORT + i;
 	  	if (!usedForwardingPorts.contains(chosenPort)) {
-	  		usedForwardingPorts.add(chosenPort);
 	  		return chosenPort;
-	  	}
-	}
+        }
+    }
+
+    return -1;
   }
 
-  bool requestForwardingConn(workerId, {Function(Socket) onDone}) {
+  void requestForwardingConn(workerId, {Function(Socket) onDone}) {
 
   	if (!workers.containsKey(workerId)) {
   		print('there is no worker with id: $workerId');
-  		return false;
+  		return;
   	}
 
   	var worker = workers[workerId];
 
   	var newPort = chooseNewForwardingPort();
+    usedForwardingPorts.add(newPort);
+    
   	print('opening port $newPort to wait the worker to make the reverse connection');
 
   	// open random port and wait for worker to make the reverse connection
   	ServerSocket.bind('0.0.0.0', newPort).then((ServerSocket server) {
 
-        // listen for incoming connections
-        server.listen((Socket workerSocket) {
-	        print('forwarding connection stablished!');
-	        onDone(workerSocket);
-        });
+      // listen for incoming connections
+      server.listen((Socket workerForwSocket) {
+	      print('forwarding connection stablished!');
+
+        // close the server (only accept the first connection)
+        server.close();
+        usedForwardingPorts.remove(newPort);
+
+        // save the socket to close it later
+        worker['forwardingSockets'].add(workerForwSocket);
+
+	      onDone(workerForwSocket);
+      });
 
 	  	// send request to the worker to ask him to connect the given port
 	  	var msg = buildOpenForwardingMsg(newPort);
@@ -167,20 +202,53 @@ class ProxyBroker {
   }
 
 
+  void removeWorker(int workerId) {
+    
+    print('removing worker: $workerId');
 
-  bool stop({Function onDone, Function onError}) {
+    if (!workers.containsKey(workerId)) {
+      print('error: worker with id: $workerId does not exist, unable to remove it');
+      return;
+    }
+
+    var worker = workers[workerId];
+
+    worker['controlSocket'].destroy();
+    worker['proxySocket'].close();
+
+    for (var socket in worker['forwardingSockets']) {
+      socket.destroy();
+    }
+
+    for (var socket in worker['browserSockets']) {
+      socket.destroy();
+    }
+
+    workers.remove(workerId);
+  }
+
+
+
+  void stop({Function onDone, Function onError}) {
+    
+    print('stopping server');
 
     if (onDone == null) {
-      onError = () => {print('server stopped')};
+      onError = () => print('server stopped');
     }
 
     if (onError == null) {
-      onError = () => {print('error stopping server')};
+      onError = () => print('error stopping server');
     }
 
+    var workerIds = workers.keys.toList();
+    for (var workerId in workerIds) {
+      removeWorker(workerId);
+    }
+    
     mainSocket.close()
-      .then((socket) => {onDone()})
-      .catchError((error) => {onError()});
+      .then((socket) => onDone())
+      .catchError((error) => onError());
   }
 }
 
--- ProxyWorker.dart
+++ ProxyWorker.dart
@@ -1,109 +1,175 @@
 import 'dart:core';
-import 'dart:async';
 import 'dart:io';
 import 'Protocol.dart';
 
 
 class ProxyWorker {
 
-	void register(brokerHost, brokerPort, exposePort) {
+	Socket controlSocket;
+  List forwardingSockets = [];
+  List remoteSockets = [];
+
+	void register(brokerHost, brokerPort, exposePort, {Function onDone, Function onError}) {
+
+		if (onDone == null) {
+		  onDone = () => print('worker registered');
+		}
+
+		if (onError == null) {
+		  onError = (e) => print('registration failed');
+		}
+
+		if (controlSocket != null) {
+			onError('already connected!');
+      return;
+		}
 
 		Socket.connect(brokerHost, brokerPort).then((Socket sock) {
+			
+			controlSocket = sock;
 
 			// send register request to the broker
 			print('sending register request to broker at $brokerHost:$brokerPort for port $exposePort');
 			List<int> msg = buildRegisterWorkerMsg(exposePort);
-			sock.add(msg);
+			controlSocket.add(msg);
 
 			// wait for messages from the broker in this "control" socket
-			sock.listen((List<int> data) {
+			controlSocket.listen((List<int> data) {
 
-	            print('incoming ${data.length} bytes from broker');
+	      print('incoming ${data.length} bytes from broker');
 
-  	            Map msg = parseProtocolMsg(data);
+  	    Map msg = parseProtocolMsg(data);
 
-	            if (msg == null) {
-	            	print('invalid msg');
-	            	return;
+        if (msg == null) {
+          print('invalid msg');
+          return;
 
-	            } else if (msg['version'] == 0x00) {
-	            	// response msg received
+        } else if (msg['version'] == 0x00) {
+          // response msg received
 
-	            	if (msg['retCode'] == RCODE_REGISTER_SUCCESS) {
-	            		print('registration succeeded');
-	            		return;
-	            	} else if (msg['retCode'] == RCODE_REGISTER_FAILED) {
-	            		print('registration failed, closing connection');
-	            		sock.close();
-	            	}
+          if (msg['retCode'] == RCODE_REGISTER_SUCCESS) {
+            print('registration succeeded');
+            onDone();
+            return;
+          } else if (msg['retCode'] == RCODE_REGISTER_FAILED) {
+            print('registration failed, closing connection');
+            controlSocket.destroy();
+            onError('registration failed');
+            return;
+          }
 
-	            } else if (msg['version'] == 0xff && msg['cmdCode'] == CMD_OPEN_FORWARDING) {
-	            	// received a request to open a reverse connection
-	            	print('request to open forwarding connection to port: ${msg['port']}');
+        } else if (msg['version'] == 0xff && msg['cmdCode'] == CMD_OPEN_FORWARDING) {
+          // received a request to open a reverse connection
+          print('request to open forwarding connection to port: ${msg['port']}');
 
-	            	Socket.connect(brokerHost, msg['port']).then((Socket forwardSocket) {
-	            		print('forwarding connection stablished!');
+          Socket.connect(brokerHost, msg['port']).then((Socket forwardSocket) {
+            print('forwarding connection stablished!');
 
-	            		bool granted = false;
-				        Socket remoteSocket = null;
+            bool granted = false;
+            Socket remoteSocket;
+            
+            // save socket to close it later
+            forwardingSockets.add(forwardSocket);
 
-	            		forwardSocket.listen((List<int> data) {
-	            			print('${data.length} bytes of data received from a forwarding connection');
+            forwardSocket.listen((List<int> data) {
+              print('${data.length} bytes of data received from a forwarding connection');
 
-	            			// NOTE: here starts the normal SOCKS protocol. The first msg should be a SOCKS4 request
-	            			// to connect to some site. The following msgs just need to be forwarded 
+              // NOTE: here starts the normal SOCKS protocol. The first msg should be a SOCKS4 request
+              // to connect to some site. The following msgs just need to be forwarded 
 
-	            			if (!granted) {
-	            				// initial state: parse received message that should be a SOCKS request
-		            			msg = parseProtocolMsg(data);
-		            			print(msg);
+              if (!granted) {
+                // initial state: parse received message that should be a SOCKS request
+                msg = parseProtocolMsg(data);
+                print(msg);
 
-	            				if (msg == null) {
-	            					print('invalid socks request, closing connection');
-	            					forwardSocket.close();
-	            					// TODO: should I return an error msg here instead of closing the connection?
-	            					return;
+                if (msg == null) {
+                  print('invalid socks request, closing connection');
+                  forwardSocket.close();
+                  // TODO: should I return an error msg here instead of closing the connection?
+                  return;
 
-	            				} else if (msg['cmdCode'] == CMD_SOCKS_OPEN_CONNECTION) {
-	            					// received a valid SOCKS request, opening a connection with the remote host
-	            					print('valid socks request, stablishing connection with remote host');
+                } else if (msg['cmdCode'] == CMD_SOCKS_OPEN_CONNECTION) {
+                  // received a valid SOCKS request, opening a connection with the remote host
+                  print('valid socks request, stablishing connection with remote host');
 
-	            					Socket.connect(msg['ip'], msg['port']).then((Socket sock) {
+                  Socket.connect(msg['ip'], msg['port']).then((Socket sock) {
 
-						                remoteSocket = sock;
+                    remoteSocket = sock;
 
-	            						remoteSocket.listen((List<int> data) {
-	            						  print('received ${data.length} bytes from remote host, retransmiting to client host');
-	            						  forwardSocket.add(data);
-	            						});
+                    // save socket to close it later
+                    remoteSockets.add(remoteSocket);
 
-	            						// connection with the remote host is stablised, inform the browser
-	            						print('connection stablished with remote host, sending back confirmation to the browser');
-	            						var resp = buildGrantedResponse();
-	            						forwardSocket.add(resp);
+                    remoteSocket.listen((List<int> data) {
+                      print('received ${data.length} bytes from remote host, retransmiting to client host');
+                      forwardSocket.add(data);
+                    }, onDone: () {
+                      remoteSocket.close();
+                    });
 
-	            						// set flag so following msgs are not interpreted as SOCKS
-	            						granted = true;
-            						});
+                    // connection with the remote host is stablised, inform the browser
+                    print('connection stablished with remote host, sending back confirmation to the browser');
+                    var resp = buildGrantedResponse();
+                    forwardSocket.add(resp);
 
-	            				} else {
-	            					// the msg is a valid SOCKS msg but is not a request. do nothing
-	            					print('valid socks msg received but is not of type request, closing the connection');
-	            					forwardSocket.close();
-	            					return;
-	            				}
+                    // set flag so following msgs are not interpreted as SOCKS
+                    granted = true;
+                  });
 
-	            			} else {
-	            				// already granted state: forward data to the remote host
-	            				print('just retransmiting');
-	            				remoteSocket.add(data);
-	            			}
-            			});
-            		});
-	            }
+                } else {
+                  // the msg is a valid SOCKS msg but is not a request. do nothing
+                  print('valid socks msg received but is not of type request, closing the connection');
+                  forwardSocket.close();
+                  return;
+                }
 
-	        });
-	    });
+              } else {
+                // already granted state: forward data to the remote host
+                print('just retransmiting');
+                remoteSocket.add(data);
+              }
+            }, onDone: () {
+              print('forwardSocket was closed by the broker, closing here');
+              forwardSocket.close();
+            });
+          });
+        }
+
+      }, onDone: () {
+        print('controlSocket was closed by the broker, stopping');
+        stop();
+      });
+    }).catchError( (e) => onError(e) );
+	}
+
+
+	void stop({Function() onDone, Function(String) onError}) {
+
+	  if (onDone == null) {
+	    onDone = () => print('worker stopped');
+	  }
+
+	  if (onError == null) {
+	    onError = (e) => print('error stopping worker: $e');
+	  }
+
+    if (controlSocket == null) {
+      onError('not connected');
+
+    } else {
+
+      for (var socket in forwardingSockets) {
+        socket.destroy();
+      }
+
+      for (var socket in remoteSockets) {
+        socket.destroy();
+      }
+
+      controlSocket.destroy();
+
+      onDone();
+      controlSocket = null;
+    }
 	}
 
 }
@@ -118,10 +184,15 @@ void main(List<String> args) async {
 		exposePort = int.parse(args[0]);
 	} 
 
-    var worker = new ProxyWorker();
+  var worker = new ProxyWorker();
 
-    worker.register('localhost', 9999, exposePort);
+  worker.register('localhost', 9999, exposePort);
 
+  // wait 5 seconds
+  // await new Future.delayed(const Duration(seconds : 5));
+
+  // disconnect worker
+  // worker.stop();
 }
 
 
